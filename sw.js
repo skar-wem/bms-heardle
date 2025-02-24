60e9c544af5a2eff16a30dce0db06d94eb4d4e4d
skar-wem <108354950+skar-wem@users.noreply.github.com>
2025-02-24 23:42:30 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('fetch', event => {
-  if (location.hostname === 'skar-wem.github.io') {
-    const url = new URL(event.request.url);
-    const redirectURL = 'https://skar.fun/bms' + url.pathname.replace('/bms-heardle', '');
-    event.respondWith(Response.redirect(redirectURL, 301));
-  }
-});
